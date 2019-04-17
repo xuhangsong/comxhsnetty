aef6edfb3545b2826b7ffa364e871532152642c1
xuhangsong <380737771@qq.com>
2019-04-17 16:37:05 +0800

    线程状态测试

---
+++ src/main/java/com/xhs/ThreadDemo.java
@@ -0,0 +1,29 @@
+package com.xhs;
+
+/**
+ * @author xuhan  build  2019/2/13
+ */
+public class ThreadDemo implements Runnable{
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new ThreadDemo());
+        System.out.println(t1.isAlive());
+        t1.start();
+        System.out.println(t1.isAlive());
+        System.out.println(t1.isAlive());
+    }
+
+    @Override
+    public void run() {
+        synchronized (Thread.currentThread()){
+            System.out.println("开始");
+            try {
+                Thread.currentThread().wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("结束");
+        }
+    }
+}
